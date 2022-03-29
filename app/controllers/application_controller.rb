717996994594d35c529606efbe59dd62e2d6be95
CKougami <100983695+CKougami@users.noreply.github.com>
2022-03-30 08:09:57 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # ログインしていないユーザーをログインページの画面に促す
+  before_action :authenticate_user!
+  # deviseのcontrollerから呼び出された場合はconfigure_permitted_parametersメソッドを呼び出す
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  # 「name」キーの内容の保存を許可する
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
