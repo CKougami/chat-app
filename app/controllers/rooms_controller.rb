d5f8eef8dd26ce4a4747d43b49e8a0a43de1a985
CKougami <100983695+CKougami@users.noreply.github.com>
2022-04-02 12:44:43 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,18 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save             # 保存が成功した場合 
+      redirect_to root_path   # ルートパスにリダイレクト
+    else                      # 保存が失敗した場合
+      render :new             #rooms/new.html.erbのページを表示
+    end
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,7 @@ class Room < ApplicationRecord
   #README!の記述どおりにアソシエーションを記述
   has_many :room_users
   has_many :users, through: :room_users
+
+  # 名前を空にしたままチャットルームを作成しようとするとエラーを起こす
+  validates :name, presence: true
 end
