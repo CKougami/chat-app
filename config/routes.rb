fe479ebf1b15a38db459b581f8a21e37c10d1441
CKougami <100983695+CKougami@users.noreply.github.com>
2022-04-02 14:11:49 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
-  root to: "messages#index"
+  # root to: "messages#index"  #ルートパスを設定（messages/index.html.erb）
+  root to: "rooms#index"  #ルートパスを設定（rooms/index.html.erb）
 
   # usersコントローラーに更新フォーム（edit）とリソース更新（update）のアクションのみ設定
   resources :users, only: [:edit, :update]
