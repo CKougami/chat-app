4424f0c9b97eb3ea5125e96cebe0dbb79022470b
CKougami <100983695+CKougami@users.noreply.github.com>
2022-04-03 18:04:50 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,13 @@ class Message < ApplicationRecord
   belongs_to :room  # 1つのメッセージは、1つのチャットルームに存在します。
   belongs_to :user  # 1つのメッセージは、1人のユーザーから送信されます。
   has_one_attached :image  #1つのメッセージに1つの画像を添付できます。
-  
+
   # content(メッセージ)が空の場合はDBに保存しない
-  validates :content, presence: true
+  # メソッドの返り値がfalseならばバリデーションによる検証を行う
+  validates :content, presence: true, unless: :was_attached?
+
+  # 画像があればtrue、なければfalseを返す
+  def was_attached?
+    self.image.attached?
+  end
 end
