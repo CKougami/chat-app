c6020770c16449f303ab1e0b7f1e1650159ee453
CKougami <100983695+CKougami@users.noreply.github.com>
2022-04-03 17:14:43 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,12 @@
 class MessagesController < ApplicationController
   def index
-    @message = Message.new               # Messageモデルのインスタンス情報を代入
-    @room = Room.find(params[:room_id])  # roomsモデルの「room_id」を代入
+    @message = Message.new               
+    @room = Room.find(params[:room_id])  
     @messages = @room.messages.includes(:user)
-
-    # ルーティングをネストしているため/rooms/:room_id/messagesというパスになる
-    # チャットルームに紐付いている全てのメッセージ（@room.messages）を@messagesと定義
-    # 一覧画面で表示するメッセージ情報には、ユーザー情報も紐付いて表示される
-    # メッセージに紐付くユーザー情報の取得でN+1問題が発生するため、includes(:user)でユーザー情報を1度のアクセスでまとめて取得している
   end
 
   def create
-    @room = Room.find(params[:room_id])  # @room = room_id
+    @room = Room.find(params[:room_id])  
     @message = @room.messages.new(message_params) #room_idとuser_idに紐づいたメッセージ
     if @message.save
       redirect_to room_messages_path(@room)  #room_idに紐づいたメッセージ一覧
@@ -24,7 +19,25 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
-    # パラメーターの中に、ログインユーザーのidと紐付いたメッセージの内容contentを受け取れるように許可。
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    # パラメーターの中に、ログインユーザーのidと紐付いたメッセージの内容[content]と[image]の引き渡しを許可。
   end
 end
+
+
+
+
+# <indexアクション>
+  # Messageモデルのインスタンス情報を代入
+  # @room = room_id
+  # ルーティングをネストしているため/rooms/:room_id/messagesというパスになる
+  # チャットルームに紐付いている全てのメッセージ（@room.messages）を@messagesと定義
+  # 一覧画面で表示するメッセージ情報には、ユーザー情報も紐付いて表示される
+  # メッセージに紐付くユーザー情報の取得でN+1問題が発生するため、includes(:user)でユーザー情報を1度のアクセスでまとめて取得している
+
+# <cerateアクション>
+  # @room = room_id
+# <destroyアクション>
+
+
+# <privateアクション>
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room  # 1つのメッセージは、1つのチャットルームに存在します。
   belongs_to :user  # 1つのメッセージは、1人のユーザーから送信されます。
+  has_one_attached :image  #1つのメッセージに1つの画像を添付できます。
   
   # content(メッセージ)が空の場合はDBに保存しない
   validates :content, presence: true
