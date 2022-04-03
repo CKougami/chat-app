ec938d90df7a1073db59e08ccf02d0643ec90fc7
CKougami <100983695+CKougami@users.noreply.github.com>
2022-04-03 15:59:23 +0900

    チャットルームの削除機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,11 +6,19 @@ class RoomsController < ApplicationController
   def create
     @room = Room.new(room_params)
     if @room.save             # 保存が成功した場合 
-      redirect_to root_path   # ルートパスにリダイレクト
+      redirect_to root_path   # top画面に戻る
     else                      # 保存が失敗した場合
       render :new             #rooms/new.html.erbのページを表示
     end
   end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
+  end
+
+  
 
   private
   def room_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"  #ルートパスを設定（rooms/index.html.erb）
 
-  # usersコントローラーのアクションを指定
   resources :users, only: [:edit, :update]
-  # roomsコントローラーのアクションを指定
-  resources :rooms, only: [:new, :create] do
-  # messagesコントローラーのアクションを指定（ネストにすることで「rooms」に属する「message」となる）
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
+
+  # usersコントローラーのアクションを指定
+  # roomsコントローラーのアクションを指定
+  # messagesコントローラーのアクションを指定（ネストにすることで「rooms」に属する「message」となる）
